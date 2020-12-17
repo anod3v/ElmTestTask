1a909fb317bb20bdba262072031fe2d98aab85f2
anod3v <anod3v@gmail.com>
2020-12-17 17:09:33 +0300

    promiseKit added

--- ElmTestTask/ElmTestTask/FeedViewController.swift
+++ ElmTestTask/ElmTestTask/FeedViewController.swift
@@ -7,15 +7,253 @@
 //
 
 import UIKit
+import PromiseKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, NewsFeedTableViewCellDelegate {
+    
+//    var networkService = NetworkService()
+    
+    var feedService = NewsFeedService()
+    
+//    var newFeedItems = [Item]()
+    
+    var newsFeedGroups = [Group]()
+    
+    var newsFeedProfiles = [Profile]()
+    
+    var photos = [String]()
+    
+    var profilesOfGroups = [ProfileInterface]()
+    
+    var profilesAndItems = [(profile: ProfileInterface?, newsItem: Item)]()
+    
+    var cellHeight: CGFloat = 0
+    
+    var cell = UITableViewCell()
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshNews), for: .valueChanged)
+        
+        return refreshControl
+    }()
+    
+    let cellID = "NewsFeedTableViewCell"
+    
+    let headerID = "NewsFeedCollectionViewHeader"
+    
+    var nextFrom = ""
+    
+    var isLoading = false
+    
+    var currentIndexPathRow: Int?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .darkGray
-        // Do any additional setup after loading the view.
+        
+        newsFeedService.getNewsFeedItems().done { result in
+            self.handleGetNewsFeedResponse(item: result.0, profile: result.1, group: result.2)
+        }
+        
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.prefetchDataSource = self
+        tableView.register(NewsFeedTableViewCell.self, forCellReuseIdentifier: cellID)
+        tableView.pin(to: view)
+
+    }
+    
+    @objc func refreshNews() {
+        
+        self.refreshControl.beginRefreshing()
+        newsFeedService.getNewsFeedItems().done { result in
+            
+            self.handleGetNewsFeedResponse(item: result.0, profile: result.1, group: result.2)
+        }
+    }
+    
+    
+    
+    func handleGetNewsFeedResponse(item: ItemResponse, profile: ProfileResponse, group: GroupResponse) {
+        let items = item.items
+        let profiles = profile.profiles
+        let groups = group.groups
+        self.nextFrom = item.nextFrom
+        
+        var profilesOrGroups = [ProfileInterface]()
+        
+//        let item = newFeedItems[indexPath.row]
+        for item in items {
+        
+        if item.sourceID! >= 0 {
+            profilesOrGroups = profiles
+        } else {
+            profilesOrGroups = groups
+        }
+        
+        let positiveSourceId = item.sourceID! >= 0 ? item.sourceID : (item.sourceID! * -1)
+        
+        let profileDataToDisplay = profilesOrGroups.first(where: {$0.id == positiveSourceId })
+            
+        let newElement = (profile: profileDataToDisplay, newsItem: item)
+            
+            if let photos = item.attachments?.compactMap({$0?.postPhoto}) {
+                cachePhotoImages(urls: photos)
+            }
+        
+        profilesAndItems.append(newElement)
+            
+        }
+        
+        DispatchQueue.main.async { self.tableView.reloadData() }
+    }
+    
+    func cachePhotoImages(urls: [String]) {
+        for url in urls {
+            PhotoService.shared.photo(url: url) { image in
+                // images saved in photoService cache
+            }
+        }
+    }
+    
+//    func handleGetNewsFeedNextResponse(item: ItemResponse, profile: ProfileResponse, group: GroupResponse) {
+//        self.newFeedItems.append(contentsOf: item.items)
+//        self.newsFeedProfiles.append(contentsOf: profile.profiles)
+//        self.newsFeedGroups.append(contentsOf: group.groups)
+//        self.nextFrom = item.nextFrom
+//
+//        DispatchQueue.main.async { self.tableView.reloadData() }
+//    }
+    
+    func revealPost(for cell: NewsFeedTableViewCell) {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+}
+
+extension FeedViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profilesAndItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! NewsFeedTableViewCell
+        
+        cell.delegate = self
+        
+        let item = profilesAndItems[indexPath.row]
+        
+//        if item.sourceID! >= 0 {
+//            profilesOfGroups = newsFeedProfiles
+//        } else {
+//            profilesOfGroups = newsFeedGroups
+//        }
+//
+//        let positiveSourceId = item.sourceID! >= 0 ? item.sourceID : (item.sourceID! * -1)
+//
+//        let dataToDisplay = profilesOfGroups.first(where: {$0.id == positiveSourceId })
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        cell.configure(item: item.newsItem)
+        cell.configureProfile(photo: item.profile?.photo, name: item.profile?.name)
+        cell.photoCollageView.isUserInteractionEnabled = false
+        
+//        currentIndexPathRow = indexPath.row
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(goToVC(_:)))
+//        cell.photoCollageView.addGestureRecognizer(tapGesture)
+        
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        guard !isLoading else { return }
+//
+//        if indexPath.row == newFeedItems.count - 1 {
+//            let lastIndex = indexPath.row
+//            isLoading = true
+//            firstly {
+//                networkService.getNewsFeedItemsWithStartTime(startFrom: nextFrom)!
+//            }.done { (result) in
+//
+//                self.handleGetNewsFeedNextResponse(item: result.0.response, profile: result.1.response, group: result.2.response)
+//
+//                let indexPaths = self.makeIndexSet(lastIndex: lastIndex, result.0.response.items.count)
+//                tableView.insertRows(at: indexPaths, with: .automatic)
+//                self.isLoading = false
+//            }.catch { (error) in
+//                debugPrint(error.localizedDescription)
+//                self.isLoading = false
+//            }
+//        }
+//    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return cell.bounds.size.height
+    }
+    
+    private func makeIndexSet(lastIndex: Int, _ newsCount: Int) -> [IndexPath] {
+        let last = lastIndex + newsCount
+        let indexPaths = Array(lastIndex + 1...last).map { IndexPath(row: $0, section: 0) }
+        
+        return indexPaths
     }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let postFullSizeViewController = PostFullSizeViewController()
+        let item = profilesAndItems[indexPath.row].newsItem
+        let likes = String(describing: item.likes?.count)
+        let text = item.text!
+        guard let photos = item.attachments?.compactMap({$0?.postPhoto}) else { return }
+        for photo in photos {
+            let photoRepresentable = PhotoRepresentable(photo: photo, likes: likes, text: text)
+            postFullSizeViewController.photos?.append(photoRepresentable)
+        }
+            postFullSizeViewController.selectedPostIndex = 0
+        
+        self.show(postFullSizeViewController, sender: nil)
+    }
+    
 }
+
+extension FeedViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        
+        for indexPath in indexPaths {
+
+        guard indexPath.row == profilesAndItems.count - 1 else { return }
+        
+        guard !isLoading else { return }
+        
+//        if indexPath.row == newFeedItems.count - 1 {
+            let lastIndex = indexPath.row
+            isLoading = true
+            firstly {
+                newsFeedService.getNewsFeedItemsWithStartTime(startFrom: nextFrom)
+            }.done { (result) in
+                
+                self.handleGetNewsFeedResponse(item: result.0, profile: result.1, group: result.2)
+                
+                let indexPaths = self.makeIndexSet(lastIndex: lastIndex, result.0.items.count)
+                tableView.insertRows(at: indexPaths, with: .automatic)
+                self.isLoading = false
+            }.catch { (error) in
+                debugPrint(error.localizedDescription)
+                self.isLoading = false
+            }
+//        }
+    }
+}
+}
+
 
---
+++ ElmTestTask/ElmTestTask/NetworkService.swift
@@ -0,0 +1,76 @@
+//
+//  NetworkService.swift
+//  ElmTestTask
+//
+//  Created by Andrey on 17/12/2020.
+//  Copyright © 2020 Andrey Anoshkin. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+class NetworkService {
+    
+    let baseHost: String = "jsonplaceholder.typicode.com"
+//    var version: String { return "5.124" }
+    let configuration = URLSessionConfiguration.default
+    
+    func getData(method: String, queryItems: [URLQueryItem] = []) -> Promise<Data> {
+        var urlConstructor = URLComponents()
+        urlConstructor.scheme = "https"
+        urlConstructor.host = self.baseHost
+        urlConstructor.path = "/\(method)"
+        
+//        urlConstructor.queryItems = [
+//            URLQueryItem(name: "user_id", value: ApiManager.session.userId),
+//            URLQueryItem(name: "access_token", value: ApiManager.session.token),
+////            URLQueryItem(name: "v", value: version),
+//        ]
+        
+        urlConstructor.queryItems?.append(contentsOf: queryItems)
+        
+        let request = URLRequest(url: urlConstructor.url!)
+        let session = URLSession(configuration: configuration)
+        
+        return Promise<Data> { seal in
+            let task = session.dataTask(with: request) { (data, response, apiError) in
+                
+                if let error = apiError {
+                    seal.reject(error)
+                }
+                
+                if let data = data {
+                    seal.fulfill(data)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func decodeData<Output: Codable>(data: Data) -> Promise<Output> {
+        
+        let decoder = JSONDecoder()
+        
+        return Promise { seal in
+            
+            do {
+                let response = try decoder.decode(VKResponse<Output>.self, from: data)
+                seal.fulfill(response.data)
+//                debugPrint(ApiManager.session.token)
+            } catch let decodingError {
+                ApiManager.session.eraseAll()
+                debugPrint(decodingError)
+                seal.reject(decodingError)
+            }
+            
+        }
+    }
+    
+    func getDataAndDecode<Output: Codable>(method: String, queryItems: [URLQueryItem] = []) -> Promise<Output> {
+        firstly {
+            getData(method: method, queryItems: queryItems)
+        }.then { (data: Data) in
+            self.decodeData(data: data)
+        }
+    }
+}
