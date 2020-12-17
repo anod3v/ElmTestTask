06926697b0eeafa9ea4d346e040cb43ffcb3f6d0
anod3v <anod3v@gmail.com>
2020-12-17 16:24:53 +0300

    storyboard removed

---
+++ ElmTestTask/ElmTestTask/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ElmTestTask
+//
+//  Created by Andrey on 17/12/2020.
+//  Copyright © 2020 Andrey Anoshkin. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
